f2eebb948e008b3bf04b618ab1e1dbdaa68766df
hzcyf <1591563365@qq.com>
2024-06-27 19:09:31 +0800

    fix some bugs

--- src/device/gemini330/G330Device.cpp
+++ src/device/gemini330/G330Device.cpp
@@ -113,7 +113,6 @@ void G330Device::initProperties() {
         if(sensor.first == OB_SENSOR_COLOR) {
             auto uvcPropertyPort = std::make_shared<UvcPropertyPort>(sourcePort);
             propertyAccessor_->registerProperty(OB_PROP_COLOR_AUTO_EXPOSURE_BOOL, "rw", "rw", uvcPropertyPort);
-            propertyAccessor_->registerProperty(OB_PROP_COLOR_AUTO_EXPOSURE_BOOL, "rw", "rw", uvcPropertyPort);
             propertyAccessor_->registerProperty(OB_PROP_COLOR_EXPOSURE_INT, "rw", "rw", uvcPropertyPort);
             propertyAccessor_->registerProperty(OB_PROP_COLOR_GAIN_INT, "rw", "rw", uvcPropertyPort);
             propertyAccessor_->registerProperty(OB_PROP_COLOR_SATURATION_INT, "rw", "rw", uvcPropertyPort);
@@ -130,11 +129,11 @@ void G330Device::initProperties() {
         }
         else if(sensor.first == OB_SENSOR_DEPTH) {
             auto uvcPropertyPort = std::make_shared<UvcPropertyPort>(sourcePort);
-            propertyAccessor_->registerProperty(OB_PROP_DEPTH_AUTO_EXPOSURE_BOOL, "rw", "rw", uvcPropertyPort);
-            propertyAccessor_->registerProperty(OB_PROP_DEPTH_EXPOSURE_INT, "rw", "rw", uvcPropertyPort);
             propertyAccessor_->registerProperty(OB_PROP_DEPTH_GAIN_INT, "rw", "rw", uvcPropertyPort);
-            // FIXME
+
             auto vendorPropertyPort = std::make_shared<VendorPropertyPort>(sourcePort);
+            propertyAccessor_->registerProperty(OB_PROP_DEPTH_AUTO_EXPOSURE_BOOL, "rw", "rw", vendorPropertyPort);
+            propertyAccessor_->registerProperty(OB_PROP_DEPTH_EXPOSURE_INT, "rw", "rw", vendorPropertyPort);
             propertyAccessor_->registerProperty(OB_PROP_LDP_BOOL, "rw", "rw", vendorPropertyPort);
             propertyAccessor_->registerProperty(OB_PROP_LASER_CONTROL_INT, "rw", "rw", vendorPropertyPort);
             propertyAccessor_->registerProperty(OB_PROP_LASER_ALWAYS_ON_BOOL, "rw", "rw", vendorPropertyPort);
@@ -406,9 +405,9 @@ DeviceResourcePtr<ISensor> G330Device::getSensor(OBSensorType sensorType) {
         auto accelProfiles = accelSensor->getStreamProfileList();
         algParamManager_->bindStreamProfileParams(accelProfiles);
 
-        LOG_ERROR("Sensor {} created! Found {} stream profiles.", OB_SENSOR_ACCEL, accelProfiles.size());
+        LOG_INFO("Sensor {} created! Found {} stream profiles.", OB_SENSOR_ACCEL, accelProfiles.size());
         for(auto &profile: accelProfiles) {
-            LOG_ERROR(" - {}", profile);
+            LOG_INFO(" - {}", profile);
         }
 
         auto gyroIter           = sensors_.find(OB_SENSOR_GYRO);
@@ -419,9 +418,9 @@ DeviceResourcePtr<ISensor> G330Device::getSensor(OBSensorType sensorType) {
         auto gyroProfiles = gyroSensor->getStreamProfileList();
         algParamManager_->bindStreamProfileParams(gyroProfiles);
 
-        LOG_ERROR("Sensor {} created! Found {} stream profiles.", OB_SENSOR_GYRO, gyroProfiles.size());
+        LOG_INFO("Sensor {} created! Found {} stream profiles.", OB_SENSOR_GYRO, gyroProfiles.size());
         for(auto &profile: gyroProfiles) {
-            LOG_ERROR(" - {}", profile);
+            LOG_INFO(" - {}", profile);
         }
     }
     else {  // type == OB_SENSOR_COLOR || type == OB_SENSOR_DEPTH || type == OB_SENSOR_IR_LEFT || type == OB_SENSOR_IR_RIGHT

a0ca427fcd640651ae79cf5f21705a66341e4bf9
bajinsi <bajinsi@orbbec.com>
2024-07-11 16:24:43 +0800

    Adaptation of hardware and software D2D switching logic and depth unit switching logic

--- src/device/component/sensor/video/DisparityBasedSensor.cpp
+++ src/device/component/sensor/video/DisparityBasedSensor.cpp
@@ -2,11 +2,13 @@
 #include "DisparityBasedSensor.hpp"
 #include "stream/StreamProfileFactory.hpp"
 #include "frame/Frame.hpp"
+#include "IProperty.hpp"
 
 namespace libobsensor {
 DisparityBasedSensor::DisparityBasedSensor(IDevice *owner, OBSensorType sensorType, const std::shared_ptr<ISourcePort> &backend)
     : VideoSensor(owner, sensorType, backend) {
     convertProfileAsDisparityBasedProfile();
+    syncDisparityToDepthModeStatus(); 
 }
 
 void DisparityBasedSensor::updateFormatFilterConfig(const std::vector<FormatFilterConfig> &configs) {
@@ -30,13 +32,37 @@ void DisparityBasedSensor::enableConvertOutputFrameAsDisparityFrame(bool enable)
     convertOutputFrameAsDisparityFrame = enable;
 }
 
+void DisparityBasedSensor::setDepthUnit(float unit){
+    depthUnit_ = unit;
+}
+
 void DisparityBasedSensor::outputFrame(std::shared_ptr<Frame> frame) {
     if(convertOutputFrameAsDisparityFrame) {
         auto sp    = frame->getStreamProfile();
         auto newSp = sp->clone();
         newSp->setFormat(OB_FORMAT_DISP16);
+        frame->setStreamProfile(newSp);
     }
+
+    auto depthFrame = frame->as<DepthFrame>();
+    if(depthFrame) {
+        depthFrame->setValueScale(depthUnit_);
+    }
+    
     VideoSensor::outputFrame(frame);
+}
+
+void DisparityBasedSensor::syncDisparityToDepthModeStatus(){
+    OBPropertyValue hwDisparityValue;
+    OBPropertyValue swDisparityValue;
+    auto commandPort = owner_->getComponentT<IPropertyPort>(OB_DEV_COMPONENT_COMMAND_PORT);
+    commandPort->getPropertyValue(OB_PROP_DISPARITY_TO_DEPTH_BOOL,&hwDisparityValue);
+    auto processor = owner_->getComponentT<FrameProcessor>(OB_DEV_COMPONENT_DEPTH_FRAME_PROCESSOR);
+    processor->getPropertyValue(OB_PROP_SDK_DISPARITY_TO_DEPTH_BOOL, &swDisparityValue);
+    if(hwDisparityValue.intValue == 1 && swDisparityValue.intValue == 1){
+        swDisparityValue.intValue = 0;
+        processor->setPropertyValue(OB_PROP_SDK_DISPARITY_TO_DEPTH_BOOL, swDisparityValue);
+    }
 }
 
 }  // namespace libobsensor
--- src/device/component/sensor/video/DisparityBasedSensor.hpp
+++ src/device/component/sensor/video/DisparityBasedSensor.hpp
@@ -11,13 +11,19 @@ public:
 
     void updateFormatFilterConfig(const std::vector<FormatFilterConfig> &configs) override;
 
+    void enableConvertOutputFrameAsDisparityFrame(bool enable);
+
+    void setDepthUnit(float unit);
 private:
     void outputFrame(std::shared_ptr<Frame> frame) override;
 
     void convertProfileAsDisparityBasedProfile();
-    void enableConvertOutputFrameAsDisparityFrame(bool enable);
+
+    void syncDisparityToDepthModeStatus();
 
 private:
     bool convertOutputFrameAsDisparityFrame = false;
+
+    float depthUnit_ = 1.0f;
 };
 }  // namespace libobsensor

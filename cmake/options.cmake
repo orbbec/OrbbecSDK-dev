2a75d3ec056f0ee3018695ac8cca5b72e546212e
yufeng <yufeng@orbbec.com>
2024-07-16 19:11:19 +0800

    Disable install target when not main project in CMake

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -7,8 +7,6 @@ option(OB_BUILD_DOCS "Build api document and install doc" ON)
 
 # platform options
 option(OB_BUILD_ANDROID "Build Android " OFF)
-option(OB_BUILD_LINUX_ARM64 "Build linux arm64, will use ARM64 toolchains on linux platform " OFF)
-option(OB_BUILD_LINUX_ARM32 "Build linux arm32, will use ARM32 toolchains on linux platform" OFF)
 
 # component options
 option(OB_BUILD_USB_PORT "Enable this to support USB/UVC/HID communication" ON)

3392f85f6ed8c8980ecbc1165eea31c8f3287d97
yufeng <yufeng@orbbec.com>
2024-07-16 16:42:04 +0800

    upload auto build scripts

---
+++ scripts/build/build_linux.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+CURRNET_DIR=$(pwd)
+
+PROJECT_ROOT=$(git rev-parse --show-toplevel)
+cd $PROJECT_ROOT
+
+# Define the platform
+ARCH=$(uname -m)
+PLATFORM="linux_$ARCH"
+echo "Building openorbbecsdk for $platform"
+
+# Variables for version and timestamp
+VERSION=$(grep -oP 'project\(\w+\s+VERSION\s+\d+\.\d+\.\d+' $PROJECT_ROOT/CMakeLists.txt | grep -oP '\d+\.\d+\.\d+')
+TIMESTAMP=$(date +"%Y%m%d%H%M")
+COMMIT_HASH=$(git rev-parse --short HEAD)
+PACKAGE_NAME="openorbbecsdk_${VERSION}_${TIMESTAMP}_${COMMIT_HASH}_${PLATFORM}"
+
+# Create build directory
+rm -rf build_$PLATFORM
+mkdir -p build_$PLATFORM
+cd build_$PLATFORM || exit
+
+# Create target directory for installation
+INSTALL_DIR=$(pwd)/install/$PACKAGE_NAME
+mkdir -p $INSTALL_DIR
+
+# Build and install openorbbecsdk
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DOB_BUILD_EXAMPLES=OFF || { echo 'Failed to run cmake'; exit 1; }
+make install -j8 || { echo 'Failed to build openorbbecsdk'; exit 1; }
+
+# Compress the installation directory
+cd $INSTALL_DIR
+cd ..
+zip -rpy ${PACKAGE_NAME}.zip $INSTALL_DIR
+
+echo "Done building and compressing openorbbecsdk for $PLATFORM"
+echo "Done building openorbbecsdk for $PLATFORM"
+
+cd $CURRNET_DIR
---
+++ scripts/build/build_linux_docker.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+CURRNET_DIR=$(pwd)
+PROJECT_ROOT=$(git rev-parse --show-toplevel)
+
+# get arch from arg
+ARCH=$1
+
+if [ "$ARCH" == "" ]; then
+    ARCH=$(uname -m)
+fi
+
+if [ "$ARCH" != "x86_64" ] && [ "$ARCH" != "aarch64" ] && [ "$ARCH" != "arm64" ]  && [ "$ARCH" != "arm" ]; then
+    echo "Invalid architecture: $ARCH, supported architectures are x86_64, aarch64, and arm"
+    exit 1
+fi
+
+platform="linux/$ARCH"
+if [ "$ARCH" == "aarch64" ]; then
+    platform="linux/arm64"
+fi
+
+echo "Building openorbbecsdk for linux $ARCH via docker"
+
+# detect docker image
+if [ "$(docker images -q openorbbecsdk-env.$ARCH 2>/dev/null)" == "" ]; then
+    echo "Docker image openorbbecsdk-env.$ARCH not found, building it with $PROJECT_ROOT/scripts/docker/$ARCH.dockerfile"
+    cd $PROJECT_ROOT/scripts/docker
+
+    # if docker buildx is not installed, install it
+    if [ "$(docker buildx ls | grep default)" == "" ]; then
+        echo "Installing docker buildx"
+        docker buildx install
+    fi
+
+    # if docker buildx plarform does not exist, create it
+    if [ "$(docker buildx ls | grep $platform)" == "" ]; then
+        echo "Creating docker buildx platform $platform"
+        docker run --privileged --rm tonistiigi/binfmt --install all
+    fi
+
+    # build docker image
+    docker buildx build --platform $platform -t openorbbecsdk-env.$ARCH -f $PROJECT_ROOT/scripts/docker/$ARCH.dockerfile . --load || {
+        echo "Failed to build docker image openorbbecsdk-env.$ARCH"
+        exit 1
+    }
+    cd $CURRNET_DIR
+fi
+
+USER_ID=$(id -u)
+GROUP_ID=$(id -g)
+
+# run docker container and build openorbbecsdk
+docker run --rm -u $USER_ID:$GROUP_ID -v $PROJECT_ROOT:/workspace -w /workspace --name OpenOrbbecSDK_Build_Liunx_$ARCH -it openorbbecsdk-env.$ARCH /bin/bash -c "cd /workspace && bash ./scripts/build/build_linux.sh"
+
+echo "Done building openorbbecsdk for linux $ARCH via docker"

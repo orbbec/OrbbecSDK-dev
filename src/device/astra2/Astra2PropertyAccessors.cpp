4ffafe4bbe4e8fffe70bf67cf850dd8043a727e1
hzcyf <1591563365@qq.com>
2024-08-27 11:01:48 +0800

    Fix g2 and astra2 device sync config

--- src/device/astra2/Astra2PropertyAccessors.cpp
+++ src/device/astra2/Astra2PropertyAccessors.cpp
@@ -130,4 +130,31 @@ void Astra2Disp2DepthPropertyAccessor::markOutputDisparityFrame(bool enable) {
     }
 }
 
+Astra2TempPropertyAccessor::Astra2TempPropertyAccessor(IDevice *owner) : owner_(owner) {}
+
+void Astra2TempPropertyAccessor::setStructureData(uint32_t propertyId, const std::vector<uint8_t> &data) {
+    utils::unusedVar(propertyId);
+    utils::unusedVar(data);
+    throw unsupported_operation_exception("Temperature params readonly!");
+}
+
+const std::vector<uint8_t> &Astra2TempPropertyAccessor::getStructureData(uint32_t propertyId) {
+    utils::unusedVar(propertyId);
+    tempData_.resize(sizeof(OBDeviceTemperature));
+
+    auto                       commandPort = owner_->getComponentT<IStructureDataAccessor>(OB_DEV_COMPONENT_MAIN_PROPERTY_ACCESSOR);
+    const std::vector<uint8_t> tempData    = commandPort->getStructureData(OB_STRUCT_DEVICE_TEMPERATURE);
+    OBDeviceTemperature        tempParam;
+    memcpy(&tempParam, tempData.data(), sizeof(OBDeviceTemperature));
+
+    tempParam.irLeftTemp     = 0;
+    tempParam.irRightTemp    = 0;
+    tempParam.chipBottomTemp = 0;
+    tempParam.chipTopTemp    = 0;
+
+    memcpy(tempData_.data(), &tempParam, sizeof(OBDeviceTemperature));
+
+    return tempData_;
+}
+
 }  // namespace libobsensor
--- src/device/gemini2/G2Device.cpp
+++ src/device/gemini2/G2Device.cpp
@@ -78,15 +78,30 @@ void G2Device::init() {
     auto depthWorkModeManager = std::make_shared<G2DepthWorkModeManager>(this);
     registerComponent(OB_DEV_COMPONENT_DEPTH_WORK_MODE_MANAGER, depthWorkModeManager);
 
-    // auto sensorStreamStrategy = std::make_shared<G2SensorStreamStrategy>(this);
-    // registerComponent(OB_DEV_COMPONENT_SENSOR_STREAM_STRATEGY, sensorStreamStrategy);
-
     static const std::vector<OBMultiDeviceSyncMode> supportedSyncModes = {
         OB_MULTI_DEVICE_SYNC_MODE_FREE_RUN,         OB_MULTI_DEVICE_SYNC_MODE_STANDALONE,          OB_MULTI_DEVICE_SYNC_MODE_PRIMARY,
         OB_MULTI_DEVICE_SYNC_MODE_SECONDARY_SYNCED, OB_MULTI_DEVICE_SYNC_MODE_SOFTWARE_TRIGGERING, OB_MULTI_DEVICE_SYNC_MODE_HARDWARE_TRIGGERING
     };
     auto deviceSyncConfigurator = std::make_shared<DeviceSyncConfiguratorOldProtocol>(this, supportedSyncModes);
     registerComponent(OB_DEV_COMPONENT_DEVICE_SYNC_CONFIGURATOR, deviceSyncConfigurator);
+    static const std::map<OBMultiDeviceSyncMode, OBSyncMode> syncModeNewToOldMap = {
+        { OB_MULTI_DEVICE_SYNC_MODE_FREE_RUN, OB_SYNC_MODE_CLOSE },  //
+        { OB_MULTI_DEVICE_SYNC_MODE_STANDALONE, OB_SYNC_MODE_STANDALONE },
+        { OB_MULTI_DEVICE_SYNC_MODE_PRIMARY, OB_SYNC_MODE_PRIMARY_MCU_TRIGGER },
+        { OB_MULTI_DEVICE_SYNC_MODE_SECONDARY, OB_SYNC_MODE_PRIMARY_IR_TRIGGER },
+        { OB_MULTI_DEVICE_SYNC_MODE_SECONDARY_SYNCED, OB_SYNC_MODE_PRIMARY_IR_TRIGGER },
+        { OB_MULTI_DEVICE_SYNC_MODE_SOFTWARE_TRIGGERING, OB_SYNC_MODE_PRIMARY },
+        { OB_MULTI_DEVICE_SYNC_MODE_HARDWARE_TRIGGERING, OB_SYNC_MODE_SECONDARY },
+    };
+    static const std::map<OBSyncMode, OBMultiDeviceSyncMode> syncModeOldToNewMap = {
+        { OB_SYNC_MODE_CLOSE, OB_MULTI_DEVICE_SYNC_MODE_FREE_RUN },  //
+        { OB_SYNC_MODE_STANDALONE, OB_MULTI_DEVICE_SYNC_MODE_STANDALONE },
+        { OB_SYNC_MODE_PRIMARY_MCU_TRIGGER, OB_MULTI_DEVICE_SYNC_MODE_PRIMARY },
+        { OB_SYNC_MODE_PRIMARY_IR_TRIGGER, OB_MULTI_DEVICE_SYNC_MODE_SECONDARY_SYNCED },
+        { OB_SYNC_MODE_PRIMARY, OB_MULTI_DEVICE_SYNC_MODE_SOFTWARE_TRIGGERING },
+        { OB_SYNC_MODE_SECONDARY, OB_MULTI_DEVICE_SYNC_MODE_HARDWARE_TRIGGERING },
+    };
+    deviceSyncConfigurator->updateModeAliasMap(syncModeOldToNewMap, syncModeNewToOldMap);
 
     registerComponent(OB_DEV_COMPONENT_DEVICE_CLOCK_SYNCHRONIZER, [this] {
         std::shared_ptr<DeviceClockSynchronizer> deviceClockSynchronizer;

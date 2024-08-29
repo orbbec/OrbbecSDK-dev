a9f9297c577fe4aa4444fcb0025956599af22a30
avicii <aiweiqi@orbbec.com>
2024-08-29 14:44:24 +0800

    Fix shell script packaging and compilation issues in win and linux

--- scripts/build/build_linux.sh
+++ scripts/build/build_linux.sh
@@ -8,20 +8,30 @@ cd $SCRIPT_DIR/../../
 PROJECT_ROOT=$(pwd)
 
 # Define the platform
-ARCH=$(uname -m)
-PLATFORM="linux_$ARCH"
+arch=$(uname -m)
+if [ "$arch" = "aarch64" ]; then
+    platform=linux_arm64
+elif [ "$arch" = "armv7l" ]; then
+    platform=linux_arm32
+else
+    platform=linux_$arch
+fi
+# PLATFORM="linux_$ARCH"
+# PLATFORM="linux_arm64"
 echo "Building openorbbecsdk for $platform"
 
 # Variables for version and timestamp
 VERSION=$(grep -oP 'project\(\w+\s+VERSION\s+\d+\.\d+\.\d+' $PROJECT_ROOT/CMakeLists.txt | grep -oP '\d+\.\d+\.\d+')
 TIMESTAMP=$(date +"%Y%m%d%H%M")
+
+git config --global --add safe.directory /workspace/OpenOrbbecSDK
 COMMIT_HASH=$(git rev-parse --short HEAD)
-PACKAGE_NAME="openorbbecsdk_${VERSION}_${TIMESTAMP}_${COMMIT_HASH}_${PLATFORM}"
+PACKAGE_NAME="openorbbecsdk_${VERSION}_${TIMESTAMP}_${COMMIT_HASH}_${platform}"
 
 # Create build directory
-rm -rf build_$PLATFORM
-mkdir -p build_$PLATFORM
-cd build_$PLATFORM || exit
+rm -rf build_$platform
+mkdir -p build_$platform
+cd build_$platform || exit
 
 # Create target directory for installation
 INSTALL_DIR=$(pwd)/install/$PACKAGE_NAME
@@ -36,7 +46,7 @@ cd $INSTALL_DIR
 cd ..
 zip -rpy ${PACKAGE_NAME}.zip ${PACKAGE_NAME} || { echo 'Failed to compress installation directory'; exit 1; }
 
-echo "Done building and compressing openorbbecsdk for $PLATFORM"
-echo "Done building openorbbecsdk for $PLATFORM"
+echo "Done building and compressing openorbbecsdk for $platform"
+echo "Done building openorbbecsdk for $platform"
 
 cd $CURRNET_DIR

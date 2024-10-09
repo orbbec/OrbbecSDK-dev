ee31aaec9bae136ce515f02862788fbfd2852ab2
hzcyf <1591563365@qq.com>
2024-10-09 19:42:06 +0800

    Fix the default value of selectedID_ in SequenceIdFilter.

--- src/filter/publicfilters/SequenceIdProcess.hpp
+++ src/filter/publicfilters/SequenceIdProcess.hpp
@@ -24,7 +24,7 @@ protected:
     std::recursive_mutex valueUpdateMutex_;
 
     // selected id; 0 for all
-    int32_t             selectedID_ = -1;
+    int32_t selectedID_ = 0;
 
     std::map<std::pair<int64_t, OBFrameType>, std::shared_ptr<Frame>> recentFrames_;
 };

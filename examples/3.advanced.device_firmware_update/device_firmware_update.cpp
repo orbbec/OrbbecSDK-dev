f6f45b3ad04e96bca95b4c4f972641112c79534f
baiye <baiye@orbbec.com>
2024-11-03 13:23:16 +0800

    Optimize terminal output and fix some bugs

--- examples/3.advanced.device_firmware_update/device_firmware_update.cpp
+++ examples/3.advanced.device_firmware_update/device_firmware_update.cpp
@@ -15,6 +15,7 @@
 
 #ifdef _WIN32
 #include <windows.h>
+#include <cstdlib>
 #else
 #include <dirent.h>
 #include <sys/stat.h>
@@ -46,52 +47,44 @@ int main() try {
 
     // Callback function for device firmware update progress
     ob::Device::DeviceFwUpdateCallback firmwareUpdateCallback = [](OBFwUpdateState state, const char *message, uint8_t percent) {
-        if(!firstCall) {
+        if(firstCall) {
             firstCall = !firstCall;
-            // Move cursor up 3 lines
-            std::cout << "\033[3F";
         }
-#ifndef _WIN32
-        // Clear the screen in linux
-        system("clear");
-#endif
-        // Clear and print the progress line
-        std::cout << "\033[K";
+        else {
+            std::cout << "\033[3F"; // Move cursor up 3 lines
+        }
+
+        std::cout << "\033[K";  // Clear the current line
         std::cout << "Progress: " << static_cast<uint32_t>(percent) << "%" << std::endl;
 
-        // Clear and print the status line
         std::cout << "\033[K";
-        std::cout << "Status: ";
+        std::cout << "Status  : ";
         switch(state) {
         case STAT_VERIFY_SUCCESS:
-            std::cout << "Image file verification success";
+            std::cout << "Image file verification success" << std::endl;
             break;
         case STAT_FILE_TRANSFER:
-            std::cout << "File transfer in progress";
+            std::cout << "File transfer in progress" << std::endl;
             break;
         case STAT_DONE:
-            std::cout << "Update completed";
+            std::cout << "Update completed" << std::endl;
             break;
         case STAT_IN_PROGRESS:
-            std::cout << "Upgrade in progress";
+            std::cout << "Upgrade in progress" << std::endl;
             break;
         case STAT_START:
-            std::cout << "Starting the upgrade";
+            std::cout << "Starting the upgrade" << std::endl;
             break;
         case STAT_VERIFY_IMAGE:
-            std::cout << "Verifying image file";
+            std::cout << "Verifying image file" << std::endl;
             break;
         default:
-            // exception will be thrown if the state is not recognized or if there is an error
-            std::cout << "Unknown status or error";
+            std::cout << "Unknown status or error" << std::endl;
             break;
         }
-        std::cout << std::endl;
 
-        // Clear and print the message line
         std::cout << "\033[K";
-        std::cout << "Message: " << message << std::endl;
-        std::cout << std::flush;
+        std::cout << "Message : " << message << std::endl << std::flush;
     };
 
     getDevice(context);
@@ -102,9 +95,10 @@ int main() try {
         if(!getFirmwarePath(firmwarePath)) {
             continue;
         }
+        std::cout << "Upgrading device firmware, please wait...\n\n";
 
         try {
-            // Update the device firmware, set async false to block until the update is complete.
+            // Set async to false to synchronously block and wait for the device firmware upgrade to complete.
             device->updateFirmware(firmwarePath.c_str(), firmwareUpdateCallback, false);
         }
         catch(ob::Error &e) {
@@ -117,8 +111,8 @@ int main() try {
         }
 
         std::cout << "Please choose one of the following options:" << std::endl;
-        std::cout << "  [R/r]           - Reboot the device to apply the firmware update\n";
-        std::cout << "  [Q/q]           - Quit the program\n";
+        std::cout << "  [R / r]         - Reboot the device to apply the firmware update\n";
+        std::cout << "  [Q / q]         - Quit the program\n";
         std::cout << "  [Any other key] - Perform another firmware update\n";
         std::cout << "---------------------------------------------------------------------------\n";
         std::cout << "Your input: ";
@@ -162,33 +156,6 @@ void onDeviceConnected(const std::string &prompt, std::shared_ptr<ob::DeviceList
 }
 
 bool getFirmwarePath(std::string &firmwarePath) {
-#if 0
-    std::cout << "\nEnter the path to the firmware file (.bin) or press 'Q' to quit: ";
-    std::getline(std::cin, firmwarePath);
-
-    if(firmwarePath == "Q" || firmwarePath == "q") {
-        exit(EXIT_SUCCESS);
-    }
-
-    if(firmwarePath.size() < 4 || firmwarePath.substr(firmwarePath.size() - 4) != ".bin") {
-        std::cerr << "Error: Invalid firmware file extension. Please provide a path to a .bin file." << std::endl;
-        std::cerr << "Example: /path/to/firmware.bin" << std::endl << std::endl;
-        return false;
-    }
-
-    std::ifstream firmwareFile(firmwarePath);
-    if(!firmwareFile) {
-        std::cerr << "Error: Firmware file not found at the specified path: " << firmwarePath << std::endl;
-        std::cerr << "Please ensure the file exists and provide the correct path." << std::endl;
-        std::cerr << "Example: /path/to/firmware.bin" << std::endl << std::endl;
-        return false;
-    }
-    firmwareFile.close();
-
-    std::cout << "Firmware file confirmed: " << firmwarePath << std::endl;
-    std::cout << "Starting firmware update, please wait..." << std::endl << std::endl;
-    return true;
-#endif
     std::string              currentPath = ".";
     std::vector<std::string> entries;
 
@@ -197,12 +164,16 @@ bool getFirmwarePath(std::string &firmwarePath) {
         listBinFiles(currentPath, entries);
 
         if(entries.empty()) {
-            std::cerr << "No firmware (.bin) files found in the current directory." << std::endl;
-            std::cout << "To retry, place the firmware file in the current directory." << std::endl;
-            std::cout << "Enter 'Q' or 'q' to quit, or press any other key to retry: ";
-        
-            char input = ob_smpl::waitForKeyPressed();
-            if(input == 'Q' || input == 'q') {
+            std::cerr << "Error: No firmware (.bin) files found in the current directory." << std::endl;
+            std::cout << "Please follow these steps:" << std::endl;
+            std::cout << "  1. Place the (.bin) file in the current program directory." << std::endl;
+            std::cout << "  2. After completing step 1, enter any key to retry." << std::endl;
+            std::cout << "      Or, enter 'Q' or 'q' to quit." << std::endl;
+            std::cout << "==================================================================" << std::endl;
+
+            std::string input;
+            std::getline(std::cin, input);
+            if(input == "Q" || input == "q") {
                 exit(EXIT_SUCCESS);
             }
             else {
@@ -211,34 +182,37 @@ bool getFirmwarePath(std::string &firmwarePath) {
         }
 
         // Display all .bin files
-        std::cout << "\nFound the following firmware files (.bin):" << std::endl;
+        std::cout << "Found the following firmware files (.bin):" << std::endl;
         for(size_t i = 0; i < entries.size(); ++i) {
             std::cout << "  [" << i << "] " << entries[i] << std::endl;
         }
 
-        std::cout << "\nEnter the corresponding index number to select the firmware file, or enter 'Q' to quit: ";
+        std::cout << "\nEnter the corresponding index number to select the firmware file, or enter 'Q' or 'q' to quit: ";
         std::string input;
         std::getline(std::cin, input);
 
-        // User chooses to quit
         if(input == "Q" || input == "q") {
             exit(EXIT_SUCCESS);
         }
 
-        // If input is an index, automatically select the corresponding file
-        if(!input.empty() && std::all_of(input.begin(), input.end(), ::isdigit)) {
-            int index = std::stoi(input);
-            if(index >= 0 && index < static_cast<int>(entries.size())) {
-                firmwarePath = currentPath + "/" + entries[index];
-                std::cout << "Firmware file confirmed: " << firmwarePath << std::endl;
-                return true;
+        try {
+            if(!input.empty() && std::all_of(input.begin(), input.end(), ::isdigit)) {
+                int index = std::stoi(input); // exception
+                if(index >= 0 && index < static_cast<int>(entries.size())) {
+                    firmwarePath = currentPath + "/" + entries[index];
+                    std::cout << "Firmware file confirmed: " << firmwarePath << std::endl;
+                    return true;
+                }
+                else {
+                    std::cerr << "Error: Invalid index." << std::endl;
+                    continue;
+                }
             }
             else {
-                std::cerr << "Error: Invalid index." << std::endl;
-                continue;
+                std::cerr << "Error: Invalid input, please enter a valid index number." << std::endl;
             }
         }
-        else {
+        catch(...) {
             std::cerr << "Error: Invalid input, please enter a valid index number." << std::endl;
         }
     }
@@ -246,7 +220,7 @@ bool getFirmwarePath(std::string &firmwarePath) {
 
 void getDevice(const std::shared_ptr<ob::Context> &context) {
     if(firstMainCall) {
-        firstMainCall                              = false;
+        firstMainCall                              = !firstMainCall;
         std::shared_ptr<ob::DeviceList> deviceList = context->queryDeviceList();
         if(deviceList->getCount() == 0) {
             // Wait for device connection
@@ -261,7 +235,7 @@ void getDevice(const std::shared_ptr<ob::Context> &context) {
             // Get the first device in the list
             device = deviceList->getDevice(0);
             std::cout << "Device found: " << device->getDeviceInfo()->getName() << std::endl;
-            std::cout << "Current device firmware version: " << device->getDeviceInfo()->getFirmwareVersion() << std::endl;
+            std::cout << "Current device firmware version: " << device->getDeviceInfo()->getFirmwareVersion() << std::endl << std::endl;
         }
     }
     else {
@@ -269,15 +243,15 @@ void getDevice(const std::shared_ptr<ob::Context> &context) {
             std::unique_lock<std::mutex> mtx(mutex);
             cond.wait(mtx, [&]() { return rebootDone; });
         }
+
         // Get the DeviceList again to update the device pointer
         std::shared_ptr<ob::DeviceList> deviceList = context->queryDeviceList();
         device                                     = deviceList->getDevice(0);
         std::shared_ptr<ob::DeviceInfo> deviceInfo = device->getDeviceInfo();
 
-        // Clear the screen
-        std::cout << "\033[2J\033[H";
+        std::cout << "\033[2J\033[H";  // Clear the screen
         std::cout << "Device reboot completed, Device name: " << deviceInfo->getName() << std::endl;
-        std::cout << "Current device firmware version: " << deviceInfo->getFirmwareVersion() << std::endl;
+        std::cout << "Current device firmware version: " << deviceInfo->getFirmwareVersion() << std::endl << std::endl;
     }
 }
 
@@ -286,15 +260,17 @@ void listBinFiles(const std::string &path, std::vector<std::string> &entries) {
 
 #ifdef _WIN32
     WIN32_FIND_DATA findFileData;
-    HANDLE          hFind = FindFirstFile((path + "\\*.bin").c_str(), &findFileData);
+    HANDLE          hFind = FindFirstFileW((std::wstring(path.begin(), path.end()) + L"\\*.bin").c_str(), &findFileData);
 
     if(hFind == INVALID_HANDLE_VALUE) {
-        std::cerr << "Can not find any .bin files in directory: " << path << std::endl;
         return;
     }
 
     do {
-        std::string name = findFileData.cFileName;
+        std::wstring wname = findFileData.cFileName;
+        size_t       len   = wname.length();
+        std::string  name(len, '\0');
+        std::wcstombs(&name[0], wname.c_str(), len);
         entries.push_back(name);
     } while(FindNextFile(hFind, &findFileData) != 0);
 
@@ -316,6 +292,5 @@ void listBinFiles(const std::string &path, std::vector<std::string> &entries) {
         std::cerr << "Can not open directory: " << path << std::endl;
     }
 #endif
-
     std::sort(entries.begin(), entries.end());
 }

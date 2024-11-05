961b51ea919dcfa46479aad1c15d58595f2f9ba6
baiye <baiye@orbbec.com>
2024-11-05 11:33:32 +0800

    Unified code formatting

--- examples/4.misc.logger/logger.cpp
+++ examples/4.misc.logger/logger.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 #include <functional>
 
-int  main() try {
+int main() try {
     // Configure the log level output to the terminal.
     ob::Context::setLoggerToConsole(OB_LOG_SEVERITY_ERROR);
 
@@ -13,25 +13,24 @@ int  main() try {
     ob::Context::setLoggerToFile(OB_LOG_SEVERITY_DEBUG, "Log/Custom/");
 
     // Register a log callback, you can get log information in the callback.
-    ob::Context::setLoggerToCallback(OB_LOG_SEVERITY_DEBUG, [](OBLogSeverity severity, const char *logMsg) {
-        std::cout << "[CallbackMessage][Level:" << severity << "]" << logMsg;
-    });
+    ob::Context::setLoggerToCallback(OB_LOG_SEVERITY_DEBUG,
+                                     [](OBLogSeverity severity, const char *logMsg) { std::cout << "[CallbackMessage][Level:" << severity << "]" << logMsg; });
 
     // Configure which streams to enable or disable for the Pipeline by creating a Config
-    std::shared_ptr<ob::Config>   config = std::make_shared<ob::Config>();
+    std::shared_ptr<ob::Config> config = std::make_shared<ob::Config>();
     config->enableVideoStream(OB_STREAM_DEPTH, OB_WIDTH_ANY, OB_HEIGHT_ANY, OB_FPS_ANY, OB_FORMAT_Y16);
     config->enableVideoStream(OB_STREAM_COLOR, OB_WIDTH_ANY, OB_HEIGHT_ANY, OB_FPS_ANY, OB_FORMAT_RGB);
 
     // Create a pipeline with default device to manage stream
-    std::shared_ptr<ob::Pipeline> pipe   = std::make_shared<ob::Pipeline>();
+    std::shared_ptr<ob::Pipeline> pipe = std::make_shared<ob::Pipeline>();
 
     // Start the pipeline with config
     pipe->start(config);
     // Stop the Pipeline, no frame data will be generated
     pipe->stop();
 
-	ob::Context::setLoggerToCallback(OB_LOG_SEVERITY_OFF, nullptr);
-	std::cout << "\nPress any key to exit.";
+    ob::Context::setLoggerToCallback(OB_LOG_SEVERITY_OFF, nullptr);
+    std::cout << "\nPress any key to exit.";
     ob_smpl::waitForKeyPressed();
     return 0;
 }
